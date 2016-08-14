c3a41329f19cf67a9f2060241b669a822c3d05ed
Evans Jahja <evans.jahja@yahoo.com>
2016-08-14 20:38:06 +0700

    Rewrote history, removed ba92c9e6e4d0 because lack of gitignore

---
+++ Assets/Other/Other Resources/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.aspect = 720f / 1280f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Other/Other Resources/Scripts/NumpadScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumpadScript : MonoBehaviour {
+	//variables for box 1 and box 2
+
+	string display = "";
+
+	public void ButtonOnClick(GameObject button) {
+		display = button.name;
+		Debug.Log (display);
+		updateText (display);
+	}
+
+	void EnterOnClick() {
+
+	}
+
+	void SignOnClick() {
+
+	}
+
+	void updateText(string newText) {
+		Text angka = gameObject.GetComponent<Text> ();
+		angka.text = newText;
+	}
+}
