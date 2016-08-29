c8a6711ff6ae9a4ce9b8ea8f56316de2fd7c68ca
ervina <eindivinereinherjar@gmail.com>
2016-08-30 03:11:35 +0700

    various updates
    + more digits
    + UI updates
    + bank soal

--- Assets/Other/Other Resources/Image/Scripts/NumpadScript.cs
+++ Assets/Other/Other Resources/Image/Scripts/NumpadScript.cs
@@ -6,17 +6,25 @@ public class NumpadScript : MonoBehaviour {
 	//variables for box 1 and box 2
 	public GameObject AngkaA, AngkaB;
 	GameObject currentAngka;
+	bool _switch;
 
 	string display = "";
 
 	void Start() {
 		currentAngka = AngkaA;
 		currentAngka.GetComponent<Text>().color = Color.yellow;
+		_switch = true;
 	}
 
+	//function for numpad button on click
 	public void ButtonOnClick(GameObject button) {
-		display = button.name;
-		Debug.Log (display);
+		if (_switch) {
+			display = button.name;
+			_switch = false;
+		} else if(display.Length<3) {
+			display = display + button.name;
+		}
+		Debug.Log (display.Length);
 		updateText (display);
 	}
 
@@ -26,6 +34,7 @@ public class NumpadScript : MonoBehaviour {
 
 	public void SignOnClick() {
 		//toggle between AngkaA and AngkaB
+		_switch = true;
 		currentAngka.GetComponent<Text>().color = Color.white;
 		Debug.Log("sign");
 		if (currentAngka == AngkaA)
---
+++ Assets/Other/Other Resources/Image/Scripts/QuestionSet.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class QuestionSet : MonoBehaviour {
+	int questionLength = 12;
+	int selectedQuestion;
+	System.Random rnd;
+
+	// Use this for initialization
+	void Start () {
+		rnd = new System.Random ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpinQuestionWheel() {
+		selectedQuestion = rnd.Next (questionLength);
+	}
+
+	public int GetAnswer(int a, int b) {
+		int result = -666;
+		switch(selectedQuestion) {
+		case 1:
+			result = a + b;
+			break;
+		case 2: //sum of digits
+			result = a;
+			break;
+		case 3:
+			result = Mathf.Abs (a - b);
+			break;
+		case 4:
+			result = a * b;
+			break;
+		case 5: //a/b
+			result = b;
+			break;
+		case 6:
+			result = a % b;
+			break;
+		case 7:
+			result = b % a;
+			break;
+		case 8:
+			result = (a + b) % 2;
+			break;
+		case 9:
+			if (a > b)
+				result = a;
+			else 
+				result = b;
+			break;
+		case 10:
+			if (a < b)
+				result = a;
+			else 
+				result = b;
+			break;
+		case 11:
+			if (a > b)
+				result = 1;
+			else
+				result = 0;
+			break;
+		case 12:
+			if (a < b)
+				result = 1;
+			else
+				result = 0;
+			break;
+		};
+		return result;
+	}
+}
